ded673cd55b50f8a3e98bbd226a0f96fb7644ddb
simon.goodchild <simon.goodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-29 19:57:08 +0000

    0.56.4 Updated core wps_display_name function to support WPML plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-symposium-pro/tags/0.56.4@905011 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ forums/wps_forum_edit.php
@@ -0,0 +1,187 @@
+<?php
+
+function wps_post_edit($post_id, $atts) {
+
+	global $current_user;
+
+	$html = '';
+
+	$the_post = get_post($post_id);
+
+	if ($current_user->ID == $the_post->post_author || current_user_can('manage_options')):
+
+		// Shortcode parameters
+		extract( shortcode_atts( array(
+			'class' => '',
+			'title_label' => 'Post title',
+			'content_label' => 'Post',
+			'cancel_label' => __('Cancel', WPS2_TEXT_DOMAIN),
+			'update_label' => __('Update Topic', WPS2_TEXT_DOMAIN),
+			'moderate_msg' => __('Your post will appear once it has been moderated.', WPS2_TEXT_DOMAIN),
+			'moderate' => false,
+			'slug' => '',
+			'before' => '',
+			'after' => '',
+		), $atts, 'wps_forum_post' ) );
+
+		$form_html = '';
+		$form_html .= '<div id="wps_forum_post_edit_div">';
+			
+			$form_html .= '<div id="wps_forum_post_edit_form">';
+
+				$url = wps_curPageURL();
+				$url = preg_replace("/[&?]forum_action=edit&post_id=[0-9]+/","",$url);
+
+				$form_html .= '<form ACTION="'.$url.'" METHOD="POST">';
+				$form_html .= '<input type="hidden" name="action" value="wps_forum_post_edit" />';
+				$form_html .= '<input type="hidden" name="wps_post_id" value="'.$post_id.'" />';
+				$form_html .= '<input type="hidden" name="wps_forum_moderate" value="'.$moderate.'" />';
+
+				$form_html .= '<div id="wps_forum_post_title_label">'.$title_label.'</div>';
+				$form_html .= '<input type="text" id="wps_forum_post_edit_title" name="wps_forum_post_edit_title" value="'.$the_post->post_title.'" />';
+
+				$form_html .= '<div id="wps_forum_post_content_label">'.$content_label.'</div>';
+				$form_html = apply_filters( 'wps_forum_post_pre_form_filter', $form_html, $atts, $current_user->ID );
+				$form_html .= '<textarea id="wps_forum_post_edit_textarea" name="wps_forum_post_edit_textarea">'.$the_post->post_content.'</textarea>';
+
+				if ($moderate) $form_html .= '<div id="wps_forum_post_edit_moderate">'.$moderate_msg.'</div>';
+				$form_html = apply_filters( 'wps_forum_post_post_form_filter', $form_html, $atts, $current_user->ID );
+
+			$form_html .= '</div>';
+
+			$form_html .= '<input id="wps_forum_post_edit_button" type="submit" class="'.$class.'" value="'.$update_label.'" />';
+			$form_html .= '</form>';
+			$form_html .= '<form ACTION="'.$url.'" METHOD="POST">';
+				$form_html .= '<input id="wps_forum_post_cancel_button" type="submit" class="'.$class.'" value="'.$cancel_label.'" />';
+			$form_html .= '</form>';
+		
+		$form_html .= '</div>';
+
+		$html .= $form_html;
+
+	else:
+
+		$html .= __('Not the post owner', WPS2_TEXT_DOMAIN);
+
+	endif;
+
+	return $html;
+
+}
+
+function wps_comment_edit($comment_id, $atts) {
+
+	global $current_user;
+
+	$html = '';
+
+	$the_comment = get_comment($comment_id);
+
+	if ($current_user->ID == $the_comment->user_id || current_user_can('manage_options')):
+
+		// Shortcode parameters
+		extract( shortcode_atts( array(
+			'class' => '',
+			'content_label' => '',
+			'cancel_label' => __('Cancel', WPS2_TEXT_DOMAIN),
+			'update_label' => __('Update Comment', WPS2_TEXT_DOMAIN),
+			'moderate' => false,
+			'moderate_msg' => __('Your comment will appear once it has been moderated.', WPS2_TEXT_DOMAIN),
+			'slug' => '',
+			'before' => '',
+			'after' => '',
+		), $atts, 'wps_forum_comment' ) );
+
+		$form_html = '';
+		$form_html .= '<div id="wps_forum_post_edit_div">';
+			
+			$form_html .= '<div id="wps_forum_post_edit_form">';
+
+				$url = wps_curPageURL();
+				$url = preg_replace("/[&?]forum_action=edit&comment_id=[0-9]+/","",$url);
+
+				$form_html .= '<form ACTION="'.$url.'" METHOD="POST">';
+				$form_html .= '<input type="hidden" name="action" value="wps_forum_comment_edit" />';
+				$form_html .= '<input type="hidden" name="wps_comment_id" value="'.$comment_id.'" />';
+				$form_html .= '<input type="hidden" name="wps_forum_moderate" value="'.$moderate.'" />';
+
+				$form_html .= '<div id="wps_forum_comment_content_label">'.$content_label.'</div>';
+				$form_html = apply_filters( 'wps_forum_comment_edit_pre_form_filter', $form_html, $atts, $current_user->ID );
+				$form_html .= '<textarea id="wps_forum_comment_edit_textarea" name="wps_forum_comment_edit_textarea">'.$the_comment->comment_content.'</textarea>';
+
+				if ($moderate) $form_html .= '<div id="wps_forum_comment_edit_moderate">'.$moderate_msg.'</div>';
+				$form_html = apply_filters( 'wps_forum_comment_edit_post_form_filter', $form_html, $atts, $current_user->ID );
+
+			$form_html .= '</div>';
+
+			$form_html .= '<input id="wps_forum_comment_edit_button" type="submit" class="'.$class.'" value="'.$update_label.'" />';
+			$form_html .= '</form>';
+			$form_html .= '<form ACTION="'.$url.'" METHOD="POST">';
+				$form_html .= '<input id="wps_forum_post_cancel_button" type="submit" class="'.$class.'" value="'.$cancel_label.'" />';
+			$form_html .= '</form>';
+		
+		$form_html .= '</div>';
+
+		$html .= $form_html;
+
+	else:
+
+		$html .= __('Not the comment owner', WPS2_TEXT_DOMAIN);
+
+	endif;
+
+	return $html;
+
+}
+
+function wps_save_post($post_data, $files_data) {
+
+	global $current_user;
+	
+	$post_id = $post_data['wps_post_id'];
+	if ($post_id):
+
+		$current_post = get_post($post_id);
+		if ( $current_user->ID == $current_post->post_author || current_user_can('manage_options') ):
+
+		  	$my_post = array(
+		      	'ID'           	=> $post_id,
+		      	'post_title' 	=> $post_data['wps_forum_post_edit_title'],
+		      	'post_content' 	=> $post_data['wps_forum_post_edit_textarea'],
+		  	);
+		  	wp_update_post( $my_post );				
+
+			// Any further actions?
+			do_action( 'wps_forum_post_edit_hook', $post_data, $files_data, $post_id );
+
+		endif;
+
+	endif;
+
+}
+
+function wps_save_comment($post_data, $files_data) {
+
+	global $current_user;
+	
+	$comment_id = $post_data['wps_comment_id'];
+	if ($comment_id):
+
+		$current_comment = get_comment($comment_id);
+		if ( $current_user->user_login == $current_comment->comment_author || current_user_can('manage_options') ):
+
+			$commentarr = array();
+			$commentarr['comment_ID'] = $comment_id;
+			$commentarr['comment_content'] = $post_data['wps_forum_comment_edit_textarea'];
+			wp_update_comment( $commentarr );			
+
+			// Any further actions?
+			do_action( 'wps_forum_comment_edit_hook', $post_data, $files_data, $comment_id );
+
+		endif;
+
+	endif;
+
+}
+
+?>
---
+++ forums/wps_forum_posts.php
@@ -0,0 +1,185 @@
+<?php
+
+$term = get_term_by('slug', $slug, 'wps_forum');
+$term_children = get_term_children($term->term_id, 'wps_forum');
+
+
+if ($show_header):
+	$html .= '<div class="wps_forum_posts_header">';
+		$html .= '<div class="wps_forum_title_header">'.__('Post', WPS2_TEXT_DOMAIN).'</div>';
+		$html .= '<div class="wps_forum_count_header">'.__('Replies', WPS2_TEXT_DOMAIN).'</div>';
+		$html .= '<div class="wps_forum_last_poster_header">'.__('Last activity', WPS2_TEXT_DOMAIN).'</div>';
+	$html .= '</div>';
+endif;
+
+$forum_posts = array();
+global $post, $current_user;
+
+// Get posts
+$loop = new WP_Query( array(
+	'post_type' => 'wps_forum_post',
+	'posts_per_page' => -1,
+	'post_status' => array ('publish', 'pending'),
+	'tax_query' => array(
+		'relation' => 'AND',
+		array(
+			'taxonomy' => 'wps_forum',
+			'field' => 'slug',
+			'terms' => $slug,
+		),
+		array( 
+			'taxonomy' => 'wps_forum',
+			'field' => 'id',
+			'terms' => $term_children,
+			'operator' => 'NOT IN'
+			)
+	    ),
+) );
+
+
+if ($loop->have_posts()):
+	while ( $loop->have_posts() ) : $loop->the_post();
+
+		$forum_post = array();
+		$forum_post['ID'] = $post->ID;
+		$forum_post['post_status'] = $post->post_status;
+		$forum_post['post_author'] = $post->post_author;
+		$forum_post['post_name'] = $post->post_name;
+		$forum_post['post_title'] = $post->post_title;
+		$forum_post['post_date'] = $post->post_date;
+		$forum_post['post_date_gmt'] = $post->post_date_gmt;
+		$forum_post['comment_status'] = $post->comment_status;
+		$forum_post['is_sticky'] = get_post_meta($post->ID, 'wps_sticky');
+
+		$args = array(
+			'status' => 1,
+			'orderby' => 'comment_ID',
+			'number' => 1,
+			'order' => 'DESC',
+			'post_id' => $post->ID,
+		);
+		$comments = get_comments($args);
+		if ($comments):
+			$forum_post['last_comment'] = $comments[0]->comment_date;
+		else:
+			$forum_post['last_comment'] = $post->post_date;
+		endif;
+
+		$forum_posts[$post->ID] = $forum_post;
+
+	endwhile;
+
+endif;
+
+if ($forum_posts):
+
+	// Sort the posts by sticky first, then last contributed to, finally last added
+	$sort = array();
+	foreach($forum_posts as $k=>$v) {
+	    $sort['is_sticky'][$k] = $v['is_sticky'];
+	    $sort['last_comment'][$k] = $v['last_comment'];
+	    $sort['ID'][$k] = $v['ID'];
+	}
+	array_multisort($sort['is_sticky'], SORT_DESC, $sort['last_comment'], SORT_DESC, $sort['ID'], SORT_DESC, $forum_posts);
+
+	$html .= '<div class="wps_forum_posts">';
+
+		foreach ($forum_posts as $forum_post):
+
+			if ($forum_post['post_status'] == 'publish' || current_user_can('edit_posts') || $forum_post['post_author'] = $current_user->ID):
+
+				$args = array(
+					'post_id' => $forum_post['ID'],
+				    'count' => true
+				);
+				$comments_count = get_comments($args);
+
+				$args = array(
+					'status' => 1,
+					'orderby' => 'comment_ID',
+					'number' => 2,
+					'order' => 'DESC',
+					'post_id' => $forum_post['ID'],
+				);
+				$comments = get_comments($args);
+				if ($comments):
+					if ($comments[0]->user_id):
+						$author = wps_display_name(array('user_id'=>$comments[0]->user_id, 'link'=>1));
+					else:
+						if ($comments_count > 1):
+							$author = $author = wps_display_name(array('user_id'=>$comments[1]->user_id, 'link'=>1));
+						else:
+							$author = wps_display_name(array('user_id'=>$forum_post['post_author'], 'link'=>1));
+						endif;
+					endif;
+					$created = sprintf(__('%s ago', WPS2_TEXT_DOMAIN), human_time_diff(strtotime($comments[0]->comment_date_gmt), current_time('timestamp', 1)), WPS2_TEXT_DOMAIN);
+
+					$args = array(
+						'status' => 1,
+						'count' => true,
+						'post_id' => $forum_post['ID'],
+					);
+					$comment_count = get_comments($args);
+
+				else:
+					$author = wps_display_name(array('user_id'=>$forum_post['post_author'], 'link'=>1));
+					$created = sprintf(__('%s ago', WPS2_TEXT_DOMAIN), human_time_diff(strtotime($forum_post['post_date_gmt']), current_time('timestamp', 1)), WPS2_TEXT_DOMAIN);
+					$comment_count = 0;
+				endif;
+
+				$forum_html = '';
+
+				$forum_html .= '<div class="wps_forum_post';
+					if ($forum_post['is_sticky']) $forum_html .= ' wps_forum_post_sticky';
+					$forum_html .= '">';
+					$forum_html .= '<div class="wps_forum_title">';
+						if ($forum_post['comment_status'] == 'closed' && $closed_prefix) $forum_html .= '['.$closed_prefix.'] ';
+						if ($forum_post['post_status'] == 'publish'):
+
+							if (is_multisite()) {
+
+								$blog_details = get_blog_details($blog->blog_id);
+								$url = $blog_details->path.$slug.'/'.$forum_post['post_name'];
+
+
+							} else {
+
+								$permalink_structure = get_option( 'permalink_structure' );
+								if ($permalink_structure):
+									$url = get_bloginfo('url').'/'.$slug.'/'.$forum_post['post_name'];
+								else:
+									$forum_page_id = wps_get_term_meta($term->term_id, 'wps_forum_cat_page', true);
+									$url = get_bloginfo('url')."/?page_id=".$forum_page_id."&topic=".$forum_post['post_name'];
+								endif;
+
+							}
+							
+							$forum_html .= '<a href="'.$url.'">'.esc_attr($forum_post['post_title']).'</a>';
+						else:
+							$forum_html .= esc_attr($forum_post['post_title']).' '.$pending;						
+						endif;
+					$forum_html .= '</div>';
+					$forum_html .= '<div class="wps_forum_count">'.$comment_count.'</div>';
+					$forum_html .= '<div class="wps_forum_last_poster">'.$author.'</div>';
+					$forum_html .= '<div class="wps_forum_freshness">'.$created.'</div>';
+				$forum_html .= '</div>';
+
+				$forum_html = apply_filters( 'wps_forum_post_item', $forum_html );
+				$html .= $forum_html;
+
+			endif;
+
+		endforeach;
+
+	$html .= '</div>';
+
+else:
+
+	$html .= $empty_msg;
+
+endif;
+
+wp_reset_query();
+
+?>
+
